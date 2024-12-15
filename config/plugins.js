6fb8e405426b9e37ba11e63cbc0fef81e7a0be0d
shepelenvlad <shepelenvlad@gmail.com>
2024-12-15 22:33:44 +0100

    add graphql

--- config/plugins.js
+++ config/plugins.js
@@ -19,3 +19,20 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+
+module.exports = {
+  //
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+};
